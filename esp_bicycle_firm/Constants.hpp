0ba0597575e54e60a0adca866c80c8cf9aff4ad7
Susan Yoon <awesomeyooner@gmail.com>
2024-08-11 10:09:08 -0700

    changed neutral

--- esp_bicycle_firm/Constants.hpp
+++ esp_bicycle_firm/Constants.hpp
@@ -3,7 +3,7 @@ namespace MotorConstants{
   const String DEVICE = "virtual_rear_wheel_joint";
 
   const int MAX_FORWARD = 600;
-  const int NEUTRAL = 330;
+  const int NEUTRAL = 360;
   const int MAX_REVERSE = 60;
 
   const int CHANNEL = 0;
